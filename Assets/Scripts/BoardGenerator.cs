f8319bced5a4d57b64e276f0fbd0c3930e388910
Dorotea <35565194+Dorole@users.noreply.github.com>
2022-05-17 19:34:19 +0200

    Board initialization
    A 10x10 board can be generated in editor.

---
+++ Assets/Scripts/BoardGenerator.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Battleship
+{
+    public class BoardGenerator : MonoBehaviour
+    {
+        [SerializeField] bool _generateInEditor;
+        [SerializeField] GameObject _tilePrefab;
+        [SerializeField] int _boardSizeX = 10, _boardSizeZ = 10;
+
+        List<GameObject> _tileList = new List<GameObject>();
+
+        #region GENERATE_IN_EDITOR
+        private void OnDrawGizmos()
+        {
+            if (_tilePrefab != null && _generateInEditor)
+            {
+                DeleteExistingTiles();
+                GenerateBoard();
+            }
+        }
+
+        void DeleteExistingTiles()
+        {
+            for (int i = 0; i < _tileList.Count; i++)
+                DestroyImmediate(_tileList[i]);
+
+            _tileList.Clear();
+        }
+        #endregion
+
+        private void Start()
+        {
+            //GenerateBoard();
+        }
+
+        void GenerateBoard()
+        {
+            for (int i = 0; i < _boardSizeX; i++)
+            {
+                for (int j = 0; j < _boardSizeZ; j++)
+                {
+                    Vector3 tilePos = new Vector3(transform.position.x + i, 0, transform.position.z + j);
+                    GameObject tile = Instantiate(_tilePrefab, tilePos, Quaternion.identity, transform);
+                    tile.name = $"Tile: x {i}, z {j}";
+
+                    _tileList.Add(tile); 
+                }
+            }
+        }
+    }
+}
